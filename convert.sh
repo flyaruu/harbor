917a3df5f8c9992c28c93bc860fac93453ec6af5
Frank Lyaruu <flyaruu@gmail.com>
2026-05-23 20:56:41 +0200

    fix conversion script

--- convert.sh
+++ convert.sh
@@ -1,6 +1,1 @@
-docker run -it --rm --pull always -v $(pwd):/data \
-  ghcr.io/systemed/tilemaker:master \
-  /data/data/netherlands-260519.osm.pbf \
-  --output /data/current.pmtiles
-cargo run --release -p osm_pbf_processor -- --url http://localhost:8080/data 8368-8400 5412-5421 --zoom 14 --output harbor/assets/tiles
-
+docker run -it --rm -v $(pwd):/data  ghcr.io/systemed/tilemaker:master /data/data/netherlands-260522.osm.pbf --output /data/data/current.pmtiles
